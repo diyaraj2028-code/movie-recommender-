4b4036624034ff36b8c0d0f2362077a4b674d14b
diya raj <diyaraj@diyas-MacBook-Air-2.local>
2026-07-09 15:35:06 -0400

    added get_popular_movies, get_genres, and discover_by_genre functions

---
+++ tmdb.py
@@ -0,0 +1,55 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_popular_movies(): 
+  api_url = 'https://api.themoviedb.org/3/movie/popular'
+
+
+  api_key = os.getenv("API_KEY")
+
+  params = {'api_key': api_key}
+
+  response = requests.get(api_url, params=params)
+
+  if response.status_code == 200: 
+    data = response.json()
+    return data 
+  
+  print(f"Failed to fetch data. Status code: {response.status_code}")
+  print(response.text)
+  return None
+
+def get_genres(): 
+  api_url = 'https://api.themoviedb.org/3/genre/movie/list'
+
+  api_key = os.getenv("API_KEY")
+  params = {'api_key': api_key}
+
+  response = requests.get(api_url, params=params)
+
+  if response.status_code == 200: 
+    data = response.json()
+    return data
+  print(f"Failed to fetch data. Status code: {response.status_code}")
+  print(response.text)
+  return None
+
+def discover_by_genre(genre_id): 
+  api_url = 'https://api.themoviedb.org/3/discover/movie'
+  api_key = os.getenv("API_KEY")
+  params = {'api_key': api_key, 
+            'with_genres': genre_id}
+
+  response = requests.get(api_url, params=params)
+
+  if response.status_code == 200: 
+    data = response.json()
+    return data
+  print(f"Failed to fetch data. Status code: {response.status_code}")
+  print(response.text)
+  return None
+
+
